13db7037d69bd358f25c60c11386ec948c9fd569
masjallow <94096066+masjallow@users.noreply.github.com>
2023-08-06 15:27:25 -0400

    Add files via upload

---
+++ dev/main.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+
+locals {
+    tags = {
+        CreatedBy = "Jallow"
+        Environment = "dev"
+        Department = "infrastructure"
+    }
+    
+    }
+
+
+module "creative-dev-s3-bucket" {
+    source = "../terraform-modules/s3"
+    s3-bucket-name= "${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-test-${var.environment}"
+    remote-backend-bucket = "${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-terraform-remote-state"
+    remote-backend-key = "test"
+    region = "${data.aws_region.current.name}"
+    tags = local.tags
+}
+
+variable "environment" {
+    default  = "Sandbox"
+}
+
+#290668402712-us-east-1-terraform-remote-state
---
+++ dev/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "Sandbox"
+  region = "us-east-1"
+}
