9bdf117cbc86d623c873336097de411396fa3fb8
VarunPujari <vpujari@caesars.com>
2023-08-26 19:58:34 +0200

    Adding WebServer and DB server

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,3 +1,19 @@
 output "s3-bucket-name" {
   value = module.creative-dev-s3-bucket.s3-arn
 }
+
+output "database-address" {
+  value = module.rds-db.address
+}
+
+output "database-port"{
+  value = module.rds-db.port
+}
+
+output "webserver-address" {
+  value = module.webserver.alb_dns_name
+}
+
+output "webserver-asg-name" {
+  value = module.webserver.asg_name
+}
---
+++ terraform-modules/RDS/outputs.tf
@@ -0,0 +1,6 @@
+output "address" {
+  value = aws_db_instance.creative-db.address
+}
+output "port" {
+  value = aws_db_instance.creative-db.port
+}
